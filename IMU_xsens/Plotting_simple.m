dcc4d1b75c9c770ebcabbba4e74012aaaa1e69b7
Virginia <virginia@vpn-165-124-166-95.vpn.northwestern.edu>
2017-10-11 11:21:50 -0500

    Plot Simple

---
+++ IMU_xsens/Plotting_simple.m
@@ -0,0 +1,15 @@
+%% Plot
+
+data = dlmread('20171004_IMU_test.txt','\t',2,0);
+
+time1 = data(data(:,1)==1,2);
+time2 = data(data(:,1)==2,2);
+dev1 = data(data(:,1)==1,3:end);
+dev2 = data(data(:,1)==2,3:end);
+
+figure
+plot(time1,dev1)
+hold on
+plot(time2,dev2)
+legend('Roll_1','Pitch_1','Yaw_1','Roll_2','Pitch_2','Yaw_2')
+xlabel('Time [s]'); ylabel('Angle [deg]');
