a9e3399bdeae718a8f575514c9978cc4f304a207
Virginia <virginia@vpn-165-124-167-223.vpn.northwestern.edu>
2017-10-24 16:52:36 -0500

    ori

--- IMU_xsens/motfile_jointangles.m
+++ IMU_xsens/motfile_jointangles.m
@@ -1,29 +1,14 @@
 %% Create .mot file with joint angles
-
-filenameIMU = '20171012_onarm_lat.txt';
 motname = '20171012_jointangles.mot';
 fid = fopen(motname,'wt');
 
-data = dlmread(filenameIMU,'\t',2,0);
-timeIMU1 = data(data(:,1)==1,2);
-timeIMU2 = data(data(:,1)==2,2);
-IMU1 = data(data(:,1)==1,3:end);
-IMU2 = data(data(:,1)==2,3:end);
-
-ts1 = timeseries(IMU1,timeIMU1);
-ts2 = timeseries(IMU2,timeIMU2);
-[ts1s,ts2s]=synchronize(ts1,ts2,'Intersection');
-
-timeIMU = ts1s.Time;
-IMU1 = ts1s.Data;
-IMU2 = ts2s.Data;
-
 %% Write on file
-nRows = length(timeIMU);
-nCols = size(IMU1,2)+1;
+nRows = length(IMU(1).stime);
+nCols = size(IMU,2)+1;
+
 fprintf(fid,[motname,'\nnRows=%d\nnColumns=%d\n\nUnits are S.I. units (second, meters, Newtons, ...)\nAngles are in degrees.\n\nendheader\n'],nRows,nCols);
 fprintf(fid,'time\n');
-dlmwrite(motname,[timeIMU,IMU1,IMU2],'-append','delimiter','\t');
-
+dlmwrite(motname,[IMU(1).stime,IMU(1).ori,IMU(2).ori],'-append','delimiter','\t','precision','%.6f');
+fclose(fid);
 
 
