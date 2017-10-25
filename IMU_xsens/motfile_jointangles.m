fff4e5e8638957f503455f5154ea2e6e55044636
Virginia <virginia@vpn-165-124-164-69.vpn.northwestern.edu>
2017-10-25 09:23:56 -0500

    OS

--- IMU_xsens/motfile_jointangles.m
+++ IMU_xsens/motfile_jointangles.m
@@ -1,5 +1,6 @@
 %% Create .mot file with joint angles
 motname = '20171012_jointangles.mot';
+delete motname
 fid = fopen(motname,'wt');
 
 %% Write on file
@@ -7,8 +8,25 @@ nRows = length(IMU(1).stime);
 nCols = size(IMU,2)+1;
 
 fprintf(fid,[motname,'\nnRows=%d\nnColumns=%d\n\nUnits are S.I. units (second, meters, Newtons, ...)\nAngles are in degrees.\n\nendheader\n'],nRows,nCols);
-fprintf(fid,'time\n');
-dlmwrite(motname,[IMU(1).stime,IMU(1).ori,IMU(2).ori],'-append','delimiter','\t','precision','%.6f');
+fprintf(fid,'%s\t %s\t %s\t %s\t %s\t %s\n',header{:});
+dlmwrite(motname,OS.all,'-append','delimiter','\t','precision','%.6f');
 fclose(fid);
 
+%% Get OpenSim angles
+clear OS
 
+OS.time = IMU(1).stime;
+
+OS.shoulder_flexion = IMU(1).yw;
+OS.shoulder_adduction = IMU(1).pt;
+OS.shoulder_rotation = IMU(1).rl;
+
+OS.elbow_flexion = IMU(2).yw-IMU(1).yw;
+OS.radial_pronation = IMU(2).rl-IMU(1).pt;
+
+header = fieldnames(OS);
+
+OS.all = [];
+for ii = 1:length(header)
+    OS.all = [OS.all OS.(header{ii})];
+end
