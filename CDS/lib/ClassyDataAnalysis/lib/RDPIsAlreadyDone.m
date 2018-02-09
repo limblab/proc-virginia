d03f4ab192e90e4cca7574a414d56318a9ac2ede
Virginia Casasnovas <vct1641@fsm.northwestern.edu>
2018-02-09 14:07:39 -0600

    Added Analysis

--- CDS/lib/ClassyDataAnalysis/lib/RDPIsAlreadyDone.m
+++
@@ -1,8 +0,0 @@
-function isDone=RDPIsAlreadyDone(objName,targetDirectory)
-    %checks the runDataProcessing directory structure to see if a specified
-    %variable exists:
-    if ~strcmp(targetDirectory(end),filesep)
-        targetDirectory(end+1)=filesep;
-    end
-    isDone=exist([targetDirectory,'Output_Data' ,filesep,objName,'.mat'],'file');
-end
--- CDS/lib/ClassyDataAnalysis/sampleCode/GPFAAnalysis.m
+++
@@ -1,6 +0,0 @@
-gpfaConfig.trials = nonbumpTrials.number;
-gpfaConfig.dimension = 8;
-gpfaConfig.segLength = 40;
-gpfaConfig.windows=trialStartEnd;
-set(ex.bin, 'gpfaConfig', gpfaConfig);
-ex.bin.fitGpfa;
