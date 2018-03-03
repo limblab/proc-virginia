127a89a0a8293cfd7e379ce2a1a347c805998621
Virginia Casasnovas <vct1641@fsm.northwestern.edu>
2018-03-03 07:54:37 -0600

    Merge gob

--- IMU_xsens/Plotting_IMUs.m
+++ IMU_xsens/Plotting_IMUs.m
@@ -88,7 +88,6 @@ detaillevel = round(decomplevel/4);
 plt = 1;
 
 IMU = wfiltIMU(IMU,wname,detaillevel,plt);
->>>>>>> 0b1aea4f5362f790e143283a536c308a5d012140
 
 %% Binding reset segments
 IMU = bindrst(IMU);
